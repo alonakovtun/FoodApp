a546d9a3f89ce2faf99420f43fc178c91c08630e
Alona Kovtun <alonakovtun55@gmail.com>
2021-11-19 14:20:58 +0100

    CardView

--- app/src/main/java/com/example/foodapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/foodapp/ui/home/HomeFragment.java
@@ -11,17 +11,45 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodapp.R;
+import com.example.foodapp.adapters.HomeHorAdapter;
 import com.example.foodapp.databinding.FragmentHomeBinding;
+import com.example.foodapp.models.HomeHorModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-
+    RecyclerView homeHorizontalRec;
+    List<HomeHorModel> homeHorModelList;
+    HomeHorAdapter homeHorAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+
+        homeHorModelList = new ArrayList<>();
+
+        homeHorModelList.add(new HomeHorModel(R.drawable.pizza, "Pizza"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.hamburger, "Hamburger"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.fries, "Fries"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.ice_cream, "Ice Cream"));
+        homeHorModelList.add(new HomeHorModel(R.drawable.sandwich, "Sandwich"));
+
+        homeHorAdapter = new HomeHorAdapter(getActivity(), homeHorModelList);
+        homeHorizontalRec.setAdapter(homeHorAdapter);
+        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        homeHorizontalRec.setHasFixedSize(true);
+        homeHorizontalRec.setNestedScrollingEnabled(false);
+
+
+
         return root;
     }
 
